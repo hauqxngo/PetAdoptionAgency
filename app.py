956ffe57e16dbd16ef22660d5c7bcccc2c9949b8
Hau Ngo <hngo1227@gmail.com>
2021-08-11 01:51:32 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+from forms import AddPetForm, EditPetForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
+
+debug = DebugToolbarExtension(app)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def homepage():
+    """List the pets."""
+
+    pets = Pet.query.all()
+    return render_template('pet_list.html', pets=pets)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    """Add a pet; handle adding."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k!= 'csrf_token'}
+        new_pet = Pet(**data)
+        # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f'{new_pet.name} added.')
+        return redirect('/')
+
+    else:
+        # re-render the form
+        return render_template('pet_add_form.html', form=form)
+
+
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def edit_pet(pet_id):
+    """Display/Edit a pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data 
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data 
+        db.session.commit()
+        flash(f'{pet.name} is updated.')
+
+        return redirect('/')
+
+    else: 
+        return render_template('pet_edit_form.html', form=form, pet=pet)
+
+
+@app.route('/<int:pet_id>/delete', methods=['GET', 'POST'])
+def delete_pet(pet_id):
+    """Delete an existing pet."""
+    
+    pet = Pet.query.get_or_404(pet_id)
+    db.session.delete(pet)
+    db.session.commit()
+    flash(f'{pet.name} is deleted.')
+
+    return redirect('/')
+
+
